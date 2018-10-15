eabd9c62bb7ffbdf34497757fe4544b9e360fc29
Matthew Lenarz <matthewlenarz@Matthews-MacBook-Air.local>
2018-10-15 19:14:29 -0400

    apparently rails auto-generates PKs

--- db/migrate/20181015230242_create_event_types.rb
+++ db/migrate/20181015230242_create_event_types.rb
@@ -1,7 +1,6 @@
 class CreateEventTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :event_types do |t|
-      t.primary_key :id
       t.integer :user_id
       t.string :name
 
--- db/migrate/20181015230952_create_cancel_events.rb
+++ db/migrate/20181015230952_create_cancel_events.rb
@@ -1,7 +1,6 @@
 class CreateCancelEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :cancel_events do |t|
-      t.primary_key :id
       t.integer :user_id
       t.integer :event_type_id
       t.string :invitee_name
