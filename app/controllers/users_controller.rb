5bae713054ce064f534bec79fb617c0994f78f27
Matthew Lenarz <matthewlenarz@Matthews-MacBook-Air.local>
2018-10-17 11:45:18 -0400

    glue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     # todo: check logged-in (I believe rails has middleware for this)
 
+    # todo: handle missing api_key param
+
     # todo: check to see if api is already mapped to a user row in our db.  if so, short-circuit the following
     #       logic.  otherwise, we could be opening ourselves up to an asymetric resource attack (i.e. bad guys
     #       only need to send us an api key and we're stuck waiting on a curl call.)  of course, this could also
@@ -18,17 +20,24 @@ class UsersController < ApplicationController
     #       web connections.  measure to see if this optimization is actually necessary.
 
     # curl Calendly for user info.  this will be a test of whether the key is legit.  handle the curl library's 401 error.
-    # require 'net/http'
-    # uri = URI.parse("https://calendly.com/api/v1/echo")
-
-    # render json: params['api_key']
-
+    # todo: pull curl endpoint url from config file (vs. hard-coding)
+    # todo: move curl-ing logic to another class to easier testing (using mocked external endpoints)
     response = RestClient.get(
-      "https://calendly.com/api/v1/echo", 
+      "https://calendly.com/api/v1/users/me", 
       headers = {"X-TOKEN" => params['api_key']}
     )
 
-    render json: response
+    parsed = JSON.parse(response) 
+
+    # todo: either a) be cool with a single field for 'name' or b) develope a more sophisticated way of splitting names
+    user = User.create(
+        email_address: parsed["data"]["attributes"]["email"], 
+        first_name: parsed["data"]["attributes"]["name"].split(' ')[0], 
+        last_name: parsed["data"]["attributes"]["name"].split(' ')[-1], 
+        avatar_url: parsed["data"]["attributes"]["avatar"]["url"]
+    )
+
+    render json: user
 
     # todo: handle case where key is invalid/non existant.  return 400
 
